48fc4d7ef24a86faa42ee23f01f881094bdc5729
seervipradhyumn04-ship-it <seervipradhyumn04@gmail.com>
2026-06-17 22:57:18 +0530

    Create question8.py

---
+++ question8.py
@@ -0,0 +1,12 @@
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    result = a / b
+    print("Result =", result)
+
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+
+except ValueError:
+    print("Invalid input type")
